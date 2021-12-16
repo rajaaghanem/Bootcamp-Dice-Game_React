f81434b09d75930474d8ca0db3e26acef984a33b
rajaaghanem <rajaaghanem@gmail.com>
2021-12-16 12:31:22 +0200

    add classes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
+import "./App.css";
+import React from 'react';
 import Player from "./components/Player";
 import Dice from "./components/Dice";
 
-function App() {
-  return (
-    <><div className='players-container'>
-        <div><Player title="Player 1"/></div>
-        <div><Player title="Player 2"/></div>
-    </div>
-    <div className='settings-container'>
-      <Dice/>
-      <div>hold</div>
-      </div>
-    
-    
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="players-container">
+          <div>
+            <Player title="Player 1" />
+          </div>
+          <div>
+            <Player title="Player 2" />
+          </div>
+        </div>
+        <div className="settings-container">
+          <Dice />
+          <div>hold</div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
