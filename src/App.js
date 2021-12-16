2c9cf8deaecbf6d04acb608fdab4b0be97a85f09
rajaaghanem <rajaaghanem@gmail.com>
2021-12-16 22:41:24 +0200

    add reset function

--- src/App.js
+++ src/App.js
@@ -22,42 +22,73 @@ class App extends React.Component {
     const sumDice = firstDice + secoundDice;
     this.setState((state) => {
       return state.playerTurn === 1
-        ? { currentScore1: state.currentScore1 + sumDice, dice:[firstDice,secoundDice] }
-        : { currentScore2: state.currentScore2 + sumDice, dice:[firstDice,secoundDice] };
+        ? {
+            currentScore1: state.currentScore1 + sumDice,
+            dice: [firstDice, secoundDice],
+          }
+        : {
+            currentScore2: state.currentScore2 + sumDice,
+            dice: [firstDice, secoundDice],
+          };
     });
   };
 
   handleHoldClick = () => {
     this.setState((state) => {
       return state.playerTurn === 2
-        ? { playerTurn: 1,totalScore2: state.totalScore2+state.currentScore2, currentScore2: 0 }
-        : { playerTurn: 2,totalScore1: state.totalScore1+state.currentScore1, currentScore1: 0 };
+        ? {
+            playerTurn: 1,
+            totalScore2: state.totalScore2 + state.currentScore2,
+            currentScore2: 0,
+          }
+        : {
+            playerTurn: 2,
+            totalScore1: state.totalScore1 + state.currentScore1,
+            currentScore1: 0,
+          };
+    });
+  };
+
+  resetGame = () => {
+    this.setState({
+      pointsToWin: 100,
+      dice: [0, 0],
+      playerTurn: 1,
+      currentPlayer: "player 1",
+      currentScore1: 0,
+      currentScore2: 0,
+      totalScore1: 0,
+      totalScore2: 0,
     });
   };
 
   render() {
     return (
       <>
-        <Button title="New Game" />
+        <Button title="New Game" onclick={this.resetGame} />
         <div className="players-container">
           <div>
             <Player
               title="Player 1"
-              totalScore = {`${this.state.totalScore1}`}
+              totalScore={`${this.state.totalScore1}`}
               currentScore={`${this.state.currentScore1}`}
             />
           </div>
           <div>
             <Player
               title="Player 2"
-              totalScore = {`${this.state.totalScore2}`}
+              totalScore={`${this.state.totalScore2}`}
               currentScore={`${this.state.currentScore2}`}
             />
           </div>
         </div>
         <div className="settings-container">
-          <Dice onclick={this.diceClick} firstDice={this.state.dice[0]} secoundDice={this.state.dice[1]}/>
-          <Button title="Hold" onclick={this.handleHoldClick}/>
+          <Dice
+            onclick={this.diceClick}
+            firstDice={this.state.dice[0]}
+            secoundDice={this.state.dice[1]}
+          />
+          <Button title="Hold" onclick={this.handleHoldClick} />
           <input type="text" />
         </div>
       </>
